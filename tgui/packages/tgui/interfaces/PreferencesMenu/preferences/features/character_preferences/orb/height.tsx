310d9c8957c626ddc233410a1340781c6d07363a
Ákos Balázs <profakos@gmail.com>
2024-04-27 01:35:27 +0200

    Linter for height

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/orb/height.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/orb/height.tsx
@@ -1,6 +1,15 @@
-import { FeatureChoiced, FeatureDropdownInput } from '../../base';
+import {
+  FeatureChoiced,
+  FeatureChoicedServerData,
+  FeatureValueProps,
+} from '../../base';
+import { FeatureDropdownInput } from '../../dropdowns';
 
 export const height: FeatureChoiced = {
   name: 'Height',
-  component: FeatureDropdownInput,
+  component: (
+    props: FeatureValueProps<string, string, FeatureChoicedServerData>,
+  ) => {
+    return <FeatureDropdownInput buttons {...props} />;
+  },
 };
